16d29790e80b2cbfc43e73cbd2ae10986662f3ef
rokon <rokonrabbi21@gmail.com>
2023-06-07 23:16:01 +0600

    Routes.jsx added

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,17 @@
+
+export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+            path: '/',
+            element: <Home></Home>
+        }
+       
+      ]
+    }
+
+     
+    
+  ]);
