686ac2383cb877ee6c7251293ef2d135d183a4f3
rokon <rokonrabbi21@gmail.com>
2023-06-08 23:10:54 +0600

    login page modified  added

--- src/pages/Loginpage/Loginpage.jsx
+++ src/pages/Loginpage/Loginpage.jsx
@@ -61,39 +61,36 @@ const Loginpage = () => {
                 <title>SummerCamp | Login</title>
             </Helmet>
             <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col md:flex-row-reverse">
-                    <div className="text-center md:w-1/2 lg:text-left">
-                        <h1 className="text-5xl font-bold">Login now!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                <div className="hero-content flex-col ">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl text-center font-bold ">Login now!</h1>
                     </div>
-                    <div className="card md:w-1/2 max-w-sm shadow-2xl bg-base-100">
-                        <form onSubmit={handleLogin} className="card-body">
+                    <div className="card md:w-96 max-w-sm shadow-2xl bg-slate-100">
+                        <form onSubmit={handleLogin} className="font-bold  card-body">
                             <div className="form-control">
                                 <label className="label">
-                                    <span className="label-text">Email</span>
+                                    <span className="label-text text-2xl">Email</span>
                                 </label>
-                                <input type="email" name="email" placeholder="email" className="input input-bordered" />
+                                <input type="email" name="email" placeholder="email" className="input bg-white shadow-md" />
                             </div>
                             <div className="form-control">
                                 <label className="label">
-                                    <span className="label-text">Password</span>
+                                    <span className="label-text text-2xl">Password</span>
                                 </label>
-                                <input type="password" name="password" placeholder="password" className="input input-bordered" />
-                                <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                                </label>
+                                <input type="password" name="password" placeholder="password" className="input input-bordered bg-white shadow-md" />
+        
                             </div>
                            
-                            {/* TODO: make button disabled for captcha */}
+                           
                             <div className="form-control mt-6">
                                 <input disabled={false} className="btn btn-primary" type="submit" value="Login" />
                             </div>
                         </form>
-                        <p><small>New Here? <Link to="/signup">Create an account</Link> </small></p>
+                        <p className='text-xl font-bold text-center'><small>New Here? <Link className=' text-blue-800 underline' to="/register">Create an account</Link> </small></p>
                         <div className="divider"></div>
-            <div className="w-full text-center my-4">
-                <button onClick={handleGoogleSignIn} className="btn btn-circle btn-outline">
-                    G
+            <div className="w-full text-center my-2">
+                <button onClick={handleGoogleSignIn} className="">
+                <img src="https://developers.google.com/static/identity/images/btn_google_signin_light_normal_web.png" alt="" />
                 </button>
             </div>
                     </div>
