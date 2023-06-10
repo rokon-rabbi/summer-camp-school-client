55af0d8f5be0d6368009b83fb8f0be3f1ae605cb
rokon <rokonrabbi21@gmail.com>
2023-06-11 00:51:48 +0600

    dashboard added

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -8,55 +8,50 @@ import {
   FaBook,
   FaUsers,
 } from "react-icons/fa";
+import useCart from "../Hooks/useCart";
 // import useCart from "../hooks/useCart";
 // import useAdmin from "../hooks/useAdmin";
 
 const Dashboard = () => {
-//   const [cart] = useCart();
+    const [cart] = useCart();
 
   // TODO: load data from the server to have dynamic isAdmin based on Data
   // const isAdmin = true;
-  const isAdmin = true;
+  const isAdmin = false;
 
   return (
     <div className="drawer drawer-mobile ">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content">
-        <label
-          htmlFor="my-drawer-2"
-          className="btn btn-primary drawer-button lg:hidden"
-        >
-          Open drawer
-        </label>
         <Outlet></Outlet>
       </div>
-      <div className="drawer-side bg-[#D1A054]">
+      <div className="drawer-side bg-[#004567]">
         <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-        <ul className="menu p-4 w-80">
+        <ul className="menu text-white  p-4  font-bold w-80">
           {isAdmin ? (
             <>
-              <li>
+              <li className="border-y-2 border-sky-600 pb-2 rounded-md hover:bg-[#012f46]">
                 <NavLink to="/dashboard/adminhome">
                   <FaHome></FaHome> Admin Home
                 </NavLink>
               </li>
-              <li>
+              <li className="border-y-2 border-sky-600 pb-2 rounded-md hover:bg-[#012f46]">
                 <NavLink to="/dashboard/addItem">
                   {" "}
                   <FaUtensils></FaUtensils> Add an Item
                 </NavLink>
               </li>
-              <li>
+              <li className="border-y-2 border-sky-600 pb-4 rounded-md hover:bg-[#012f46]">
                 <NavLink to="/dashboard/manageitems">
                   <FaWallet></FaWallet> Manage Items
                 </NavLink>
               </li>
-              <li>
+              <li className="border-y-2 border-sky-600 pb-2 rounded-md hover:bg-[#012f46]">
                 <NavLink to="/">
                   <FaBook></FaBook> Manage Bookings(not implemented)
                 </NavLink>
               </li>
-              <li>
+              <li className="border-y-2 border-sky-600 pb-4 rounded-md hover:bg-[#012f46]">
                 <NavLink to="/dashboard/allusers">
                   <FaUsers></FaUsers> All Users
                 </NavLink>
@@ -64,25 +59,25 @@ const Dashboard = () => {
             </>
           ) : (
             <>
-              <li>
+              <li className="border-y-2 border-sky-600 pb-4 rounded-md hover:bg-[#012f46]">
                 <NavLink to="/dashboard/userhome">
                   <FaHome></FaHome> User Home
                 </NavLink>
               </li>
-              <li>
+              <li className="border-y-2 border-sky-600 pb-4 rounded-md hover:bg-[#012f46]">
                 <NavLink to="/">
                   <FaCalendarAlt></FaCalendarAlt> Reservations
                 </NavLink>
               </li>
-              <li>
+              <li className="border-y-2 border-sky-600 pb-4 rounded-md hover:bg-[#012f46]">
                 <NavLink to="/">
                   <FaWallet></FaWallet> Payment History
                 </NavLink>
               </li>
-              <li>
+              <li className="border-y-2 border-sky-600 pb-4 rounded-md hover:bg-[#012f46]">
                 <NavLink to="/dashboard/mycart">
                   <FaShoppingCart></FaShoppingCart> My Cart
-                  <span className="badge inl badge-secondary">
+                  <span className="badge inl bg-white font-bold badge-secondary">
                     +{cart?.length || 0}
                   </span>
                 </NavLink>
@@ -91,15 +86,15 @@ const Dashboard = () => {
           )}
 
           <div className="divider"></div>
-          <li>
+          <li className=" border-y-2 border-sky-600 rounded-md hover:bg-[#012f46]">
             <NavLink to="/">
               <FaHome></FaHome> Home
             </NavLink>{" "}
           </li>
-          <li>
+          <li className="border-y-2 border-sky-600 rounded-md hover:bg-[#012f46]">
             <NavLink to="/menu"> Our Menu</NavLink>
           </li>
-          <li>
+          <li className="border-y-2 border-sky-600 rounded-md hover:bg-[#012f46]">
             <NavLink to="/order/salad">Order Food</NavLink>
           </li>
         </ul>
--- src/Shared/NavBar/NavBar.jsx
+++ src/Shared/NavBar/NavBar.jsx
@@ -1,29 +1,31 @@
-import React from 'react';
+import React from "react";
 import Logo from "../../assets/logo.png";
 import { useContext, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
-import { FaShoppingCart } from 'react-icons/fa';
+import { FaShoppingCart } from "react-icons/fa";
 import { FaSignInAlt } from "react-icons/fa";
 // import { AuthContext } from '../../Providers/AuthProvider';
-import useAuth from '../../Hooks/useAuth';
+import useAuth from "../../Hooks/useAuth";
+import useCart from "../../Hooks/useCart";
 
 const NavBar = () => {
-    const [icon, setIcon] = useState(true);
-    
+  const [icon, setIcon] = useState(true);
+  const [cart] = useCart();
+  // console.log(cart)
   // const { user, logOut } =  useContext(AuthContext);
-  const { user, logOut } =  useAuth();
+  const { user, logOut } = useAuth();
 
   const handleLogOut = () => {
     logOut()
       .then()
       .catch(error => console.log(error));
   };
-    return (
-        <>
-            <nav className="md:flex bg-slate-950 md:bg-opacity-80 backdrop-filter backdrop-blur-xm  md:fixed md:w-full z-50 top-0  p-4 md:px-20 md:justify-between   items-center ">
+  return (
+    <>
+      <nav className="md:flex bg-slate-950 md:bg-opacity-80 backdrop-filter backdrop-blur-xm  md:fixed md:w-full z-50 top-0  p-4 md:px-20 md:justify-between   items-center ">
         {icon ? (
           <FontAwesomeIcon
             className="text-3xl text-white  md:hidden inline cursor-pointer relative top-8 justify-start "
@@ -51,7 +53,7 @@ const NavBar = () => {
           />
         </Link>
         <ul
-          className={` bg-slate-900 md:bg-opacity-0  shadow-2xl md:shadow-none  md:border-none border z-10  rounded-r-md px-6 py-2 pb-16 md:space-x-10 md:flex md:static md:text-lg  absolute  text-slate-950 duration-300 ${
+          className={` bg-slate-900 md:bg-opacity-0  shadow-2xl md:shadow-none  md:border-none border z-10  rounded-r-md px-6 py-2 pb-16 md:pb-0 md:space-x-10 md:flex md:static md:text-lg  absolute  text-slate-950 duration-300 ${
             !icon ? "left-0 p-2 mt-2" : "-left-40 "
           } `}
         >
@@ -85,36 +87,34 @@ const NavBar = () => {
               Classes
             </NavLink>
           </li>
-      <li className='block md:hidden'>
-      <Link className='absolute  md:-bottom-10 top-28 z-50 right-0' to="">
-                <button className="btn  gap-2">
-                    <FaShoppingCart></FaShoppingCart>
-                    <div className="badge badge-secondary ">+{ 0}</div>
-                </button>
+          <li className="block md:hidden">
+            <Link className="absolute  md:-bottom-10 top-28 z-50 right-0" to="">
+              <button className="btn  gap-2">
+                <FaShoppingCart></FaShoppingCart>
+                <div className="badge badge-secondary ">
+                  +{cart?.length || 0}
+                </div>
+              </button>
             </Link>
-      </li>
-        
-         
-          {
-            user &&(
+          </li>
+
+          {user && (
             <>
-            
-            <li className='md:pl-0 pl-0'> 
-              <NavLink
-                to="/dashboard"
-                aria-label="Dashboard"
-                title="Dashboard"
-                className={({ isActive }) => (isActive ? "active" : "default")}
-              >
-                Dashboard
-              </NavLink>
-            </li>
+              <li className="md:pl-44 pl-0">
+                <NavLink
+                  to="/dashboard"
+                  aria-label="Dashboard"
+                  title="Dashboard"
+                  className={({ isActive }) =>
+                    isActive ? "active" : "default"
+                  }
+                >
+                  Dashboard
+                </NavLink>
+              </li>
             </>
-            )
-          }
-          
+          )}
         </ul>
-       
 
         {/* <p>{user.displayName}</p> */}
         {user ? (
@@ -132,20 +132,24 @@ const NavBar = () => {
             {/* button  */}
             <button
               onClick={handleLogOut}
-              className="hidden md:block text-slate-600  border-sky-500 px-4 hover:bg-white py-1 border-2 rounded-full hover:text-sky-600 font-bold md:text-lg  "
+              className="hidden md:block text-white  border-sky-500 px-4 hover:bg-white py-1 border-2 rounded-full hover:text-sky-600 font-bold md:text-lg  "
             >
               logout
               <FaSignInAlt className="inline ml-1 mb-1 font-bold" />
             </button>
-           
-            <Link className='absolute hidden md:block md:-bottom-10 top-28 z-50 right-0' to="">
-                <button className="btn  gap-2">
-                    <FaShoppingCart></FaShoppingCart>
-                    <div className="badge badge-secondary ">+{ 0}</div>
-                </button>
+
+            <Link
+              className="absolute hidden md:block md:-bottom-10 top-28 z-50 right-0"
+              to=""
+            >
+              <button className="btn bg-slate-900  border-none  gap-2">
+                <FaShoppingCart className="text-3xl text-[#009688]" ></FaShoppingCart>
+                <div className="badge bg-slate-200 badge-secondary ">
+                  +{cart?.length || 0}
+                </div>
+              </button>
             </Link>
           </div>
-          
         ) : (
           <Link to={"/login"}>
             <button className="hidden md:block  text-white border-sky-500 px-4 hover:bg-white py-1 border-2 rounded-full hover:text-sky-600 font-bold md:text-lg ">
@@ -177,18 +181,18 @@ const NavBar = () => {
               </div>
             </div>
           ) : (
-            <Link to={"/login"} className="left-10  text-white  hover:text-sky-600  border-sky-500 px-auto hover:bg-white  py-auto border-2 rounded-full  ml-auto relative md:w-100 w-20 bottom-10 font-bold text-sm md:md:text-lg   p-1" >
-             
-                <FaSignInAlt className="inline mr-1 font-bold" />
-                login
-              
+            <Link
+              to={"/login"}
+              className="left-10  text-white  hover:text-sky-600  border-sky-500 px-auto hover:bg-white  py-auto border-2 rounded-full  ml-auto relative md:w-100 w-20 bottom-10 font-bold text-sm md:md:text-lg   p-1"
+            >
+              <FaSignInAlt className="inline mr-1 font-bold" />
+              login
             </Link>
           )}
         </div>
       </nav>
- 
-        </>
-    );
+    </>
+  );
 };
 
 export default NavBar;
