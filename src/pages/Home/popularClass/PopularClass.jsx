f71e46b26d39d3e9f58fbb7e733645b8c70bc78e
rokon <rokonrabbi21@gmail.com>
2023-06-10 13:53:12 +0600

    popular class is loading bug fixed

--- src/pages/Home/popularClass/PopularClass.jsx
+++ src/pages/Home/popularClass/PopularClass.jsx
@@ -1,38 +1,34 @@
-import React from 'react';
-import useAxios from '../../../Hooks/useAxios';
-import { useQuery } from '@tanstack/react-query'
-import useAuth from '../../../Hooks/useAuth';
+import React from "react";
+import useAxios from "../../../Hooks/useAxios";
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "../../../Hooks/useAuth";
 // import axios from 'axios';
 
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 const PopularClass = () => {
-    const { user, loading } = useAuth();
-    // const token = localStorage.getItem('access-token');
-    const [axiosSecure] = useAxios();
-    const { refetch,isLoading, data: classes = [] } = useQuery({
-        queryKey: ['classes'],
-        enabled: !loading,
-        queryFn: async () => {
-            const res = await axiosSecure(`/classes`)
-            console.log('res from axios', res)
-            return res.data;
-        },
-    })
+  // const { user, loading } = useAuth();
+  // const token = localStorage.getItem('access-token');
+  const [axiosSecure] = useAxios();
+  const { refetch, data: classes = [] } = useQuery({
+    queryKey: ["classes"],
 
-    if (isLoading) {
-      return <div>Loading...</div>;
-    }
-  
+    queryFn: async () => {
+      const res = await axiosSecure(`/classes`);
+      console.log("res from axios", res);
+      return res.data;
+    },
+  });
 
-  
-    // Sort the data based on studentsEnrolled field
-    const sortedData = classes.sort((a, b) => b.studentsEnrolled - a.studentsEnrolled);
-  
-    // Get the top six classes
-    const topSixClasses = sortedData.slice(0, 6);
-    return (
-         <div className="flex flex-wrap p-20  -mx-4">
-      {topSixClasses.map((card,idx) => (
+  // Sort the data based on studentsEnrolled field
+  const sortedData = classes.sort(
+    (a, b) => b.studentsEnrolled - a.studentsEnrolled
+  );
+
+  // Get the top six classes
+  const topSixClasses = sortedData.slice(0, 6);
+  return (
+    <div className="flex flex-wrap p-20  -mx-4">
+      {topSixClasses.map((card, idx) => (
         <motion.div
           key={idx}
           initial={{ opacity: 0, scale: 0.8 }}
@@ -41,17 +37,25 @@ const PopularClass = () => {
           className="w-full md:w-1/3 p-4"
         >
           <div className="bg-white relative overflow-hidden rounded-lg cursor-pointer text-center shadow-lg">
-            <img src={card.image} alt={card.name} className="w-full h-48 object-cover rounded-t-lg" />
+            <img
+              src={card.image}
+              alt={card.name}
+              className="w-full h-48 object-cover rounded-t-lg"
+            />
             <div className="p-4">
               <h2 className="text-xl font-bold mb-2">{card.name}</h2>
-              <p className="text-gray-600 font-semibold  "><span>students Enrolled: </span><span className=' bg-blue-100 p-2  rounded'>{card.studentsEnrolled}</span></p>
+              <p className="text-gray-600 font-semibold  ">
+                <span>students Enrolled: </span>
+                <span className=" bg-blue-100 p-2  rounded">
+                  {card.studentsEnrolled}
+                </span>
+              </p>
             </div>
           </div>
         </motion.div>
       ))}
     </div>
-      
-    );
+  );
 };
 
 export default PopularClass;
