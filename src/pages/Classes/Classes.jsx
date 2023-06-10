4b446b87646bc080f214cbca5ef9211dbd7ff69b
rokon <rokonrabbi21@gmail.com>
2023-06-10 21:45:40 +0600

    useCart hook added

--- src/pages/Classes/Classes.jsx
+++ src/pages/Classes/Classes.jsx
@@ -1,29 +1,48 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxios from "../../Hooks/useAxios";
-import { SlLike } from "react-icons/sl";
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import useAuth from "../../Hooks/useAuth";
+import Swal from 'sweetalert2'
 const Classes = () => {
-    const{user}=useAuth();
-  const [isnotavailalbe, setIsnotavailabe] = useState(false);
+  const { user } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
   const [axiosSecure] = useAxios();
   const { refetch, data: classes = [] } = useQuery({
     queryKey: ["classes"],
 
     queryFn: async () => {
       const res = await axiosSecure(`/classes`);
-      console.log("res from axios", res);
+
       return res.data;
     },
   });
   const filterData = classes.filter(item => item.status == "approved");
+  const handleSelect = data => {
+    if (user && user.email) {
+
+    }
+    else{
+        Swal.fire({
+                title: 'Please login to order the food',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Login now!'
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  navigate('/login', {state: {from: location}})
+                }
+              })
+    }
+  };
   return (
     <div>
-        <p className="md:text-5xl mb-2 p-2 bg-zinc-50 underline decoration-wavy decoration-cyan-300 text-3xl font-extrabold text-gray-900 text-center mt-14 py-10 md:mt-40">
-          Classes
-        </p>
-      <div className=" bg-zinc-50  md:p-36 p-14 grid  md:grid-cols-3 grid-cols-1 gap-10">
+      <p className="md:text-5xl mb-2 p-2 bg-zinc-50 underline decoration-wavy decoration-cyan-300 text-3xl font-extrabold text-gray-900 text-center mt-14 py-10 md:mt-40">
+        Classes
+      </p>
+      <div className=" bg-zinc-50  md:px-36 p-14 grid  md:grid-cols-3 grid-cols-1 gap-10">
         {filterData.map((data, idx) => (
           <div>
             <div
@@ -32,7 +51,7 @@ const Classes = () => {
               } h-75 rounded overflow-hidden shadow-xl border`}
             >
               <img
-                className=" rounded-3xl shadow-sm p-5  md:h-80 h-64 w-screen"
+                className=" rounded-3xl shadow-sm p-5  md:h-72 h-64 w-screen"
                 src={data.image}
                 alt={data.name}
               />
@@ -56,18 +75,19 @@ const Classes = () => {
                     {data.studentsEnrolled}
                   </span>
                 </button>
-                <Link to={``}>
+               
                   <button
+                    onClick={() => handleSelect(data)}
                     disabled={data.availableSeats === 0}
                     className={`${
-                      data.availableSeats === 0 
+                      data.availableSeats === 0
                         ? "cursor-not-allowed"
                         : "hover:bg-blue-700"
                     } bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded`}
                   >
                     Select
                   </button>
-                </Link>
+              
               </div>
             </div>
           </div>
--- src/pages/Instructors/Instructors.jsx
+++ src/pages/Instructors/Instructors.jsx
@@ -22,7 +22,7 @@ const Instructors = () => {
             <p className="md:text-5xl mb-2 p-2 bg-zinc-50 underline decoration-wavy decoration-cyan-300 text-3xl font-extrabold text-gray-900 text-center mt-14 py-10 md:mt-40">
           Instructors
         </p>
-            <div className=" bg-zinc-50  md:p-36 p-14 grid  md:grid-cols-3 grid-cols-1 gap-10">
+            <div className=" bg-zinc-50  md:px-36 p-14 grid  md:grid-cols-3 grid-cols-1 gap-10">
         {filterData.map((data,idx) => (
           <motion.div
           key={idx}
@@ -35,7 +35,7 @@ const Instructors = () => {
         {/* lazyload image  */}
 
         <img
-          className=" rounded-3xl shadow-sm p-5  md:h-80 h-64 w-screen"
+          className=" rounded-3xl shadow-sm p-5  md:h-72 h-64 w-screen"
           src={data.image}
           alt={data.name}
           
